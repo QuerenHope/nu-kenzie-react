37b2128bbe017ca8f0312ece8c99def55e34c485
QuerenHope <querenhope@hotmail.com>
2022-12-08 12:40:26 -0300

    concluido

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,34 +1,42 @@
 import { useState } from 'react'
 import Lista from '../../components/lista'
 import Form from '../../components/form'
+import {FaTrashAlt} from 'react-icons/fa'
 
 const Dashboard = ({setloginPage}) => {
 
     const [tarefas, settarefas] = useState([])
-    const [filterProduts, setfilterProduts] = useState([])
+    const [filterProduts, setfilterProduts] = useState('')
 
-    const soma = tarefas.reduce((a, b) => a + Number(b.valor), 0)
-    const sub = tarefas.reduce((a, b) => a - Number(b.valor), 0)
-
-    function Todos(){
-        settarefas([...tarefas])
-    }
+    const valueTotal = tarefas.reduce(
+        (a, b) => (b.tipo === "Saida" ? a - +b.valor : a + +b.valor),
+        0);
 
     function filtroEntradas(){
-            const filtro =  tarefas.filter((item) => item.tipo === "Entrada")
+            const filtro =  tarefas.filter((item) => item.tipo === "Entrada" && item)
 
-            settarefas(filtro)
+            setfilterProduts(filtro)
     }
 
     function filtroSaidas(){
-        const filtro =  tarefas.filter((item) => item.tipo === "Saida")
+        const filtro =  tarefas.filter((item) => item.tipo === "Saida" && item)
 
-        settarefas(filtro)
+        setfilterProduts(filtro)
 }
+    function todos(){
+        setfilterProduts('')
+    }
+    function remover(i) {
+
+        const remov = tarefas.filter((item,index)=> index !== i)
+
+        setfilterProduts(remov)
+    }
 
     
     return(
         <>
+        
             <header className="cabecalho">
                 <figure>
                     <img src="./assets/nuKenzie.png" alt="" />
@@ -46,26 +54,36 @@ const Dashboard = ({setloginPage}) => {
                     <span>Resumo financeiro</span>
                     </div>
                     <div className='buttons'>
-                    <button id='todos' onClick={Todos}>Todos</button>
+                    <button id='todos' onClick={todos}>Todos</button>
                     <button id='entradas' onClick={filtroEntradas}>Entradas</button>
                     <button id='despesas' onClick={filtroSaidas}>Despesas</button>
                     </div>
                 </div>
                 <div className='container_list'>
-                    
+                {
+                    filterProduts === '' ? 
                     <Lista tarefas = {tarefas} settarefas={settarefas}/>
+                    :
+                    filterProduts.map((tarefas,index) => (
+                    
+                        <ul key={index} className='list'>
+                            <li className='liCards'>
+                                <h2>{tarefas.descricao}</h2>
+                                <span>{tarefas.tipo}</span>
+                                <p>{`R$ ${tarefas.valor}`}</p>
+                                <button onClick={()=>remover(index)}><FaTrashAlt/></button>
+                            </li>
+                        </ul>
+                        ))
+                }
                     
                 </div>
                 </div>
             </div>
             
                 <div className='valorTotal'>
-                <h2>Valor Total:</h2>
-
-            
-                    <p>{`R$ ${soma}`}</p> 
-                                         
-                
+                    <h2>Valor Total:</h2>
+                    <p>{`R$ ${valueTotal}`}</p>                       
                 </div>
 
             </>
